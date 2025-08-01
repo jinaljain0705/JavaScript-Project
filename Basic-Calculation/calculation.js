6211c988d2ba5264f4e6617fa0cffef23f734935
Jinal Jain <jinaljain624@gmail.com>
2025-08-01 19:25:45 +0530

    commit

--- Basic-Calculation/calculation.js
+++ Basic-Calculation/calculation.js
@@ -1,6 +1,6 @@
 let X = 50, Y = 10;
 
-console.log("==== Arithmetic Calculator ====");
+console.log("==== Basic Calculation ====");
 
 console.log(`Value of X: ${X}`);
 console.log(`Value of Y: ${Y}`);
@@ -12,5 +12,3 @@ console.log(`2. Subtraction     (X - Y) : ${X} - ${Y} = ${X - Y}`);
 console.log(`3. Multiplication  (X * Y) : ${X} * ${Y} = ${X * Y}`);
 console.log(`4. Division        (X / Y) : ${Y !== 0 ? `${X} / ${Y} = ${X / Y}` : "Cannot divide by zero"}`);
 console.log(`5. Modulus         (X % Y) : ${Y !== 0 ? `${X} % ${Y} = ${X % Y}` : "Cannot divide by zero"}`);
-
-console.log("-----------------------------");
