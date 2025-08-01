6b00b4015c537dc075a3a9134b327d7b96eaf38d
Jinal Jain <jinaljain624@gmail.com>
2025-08-01 21:59:33 +0530

    commit

--- Basic-Calculation/calculation.js
+++ Basic-Calculation/calculation.js
@@ -1,14 +1,20 @@
 let X = 50, Y = 10;
 
-console.log("==== Basic Calculation ====");
+let add = X + Y;
+let sub = X - Y;
+let mul = X * Y;
+let div = X / Y;
+let mod = X % Y;
 
-console.log(`Value of X: ${X}`);
-console.log(`Value of Y: ${Y}`);
+console.log("===== Basic Calculation =====");
 
-console.log("-----------------------------");
+console.log("Value of X:", X);
+console.log("Value of Y:", Y);
 
-console.log(`1. Addition        (X + Y) : ${X} + ${Y} = ${X + Y}`);
-console.log(`2. Subtraction     (X - Y) : ${X} - ${Y} = ${X - Y}`);
-console.log(`3. Multiplication  (X * Y) : ${X} * ${Y} = ${X * Y}`);
-console.log(`4. Division        (X / Y) : ${Y !== 0 ? `${X} / ${Y} = ${X / Y}` : "Cannot divide by zero"}`);
-console.log(`5. Modulus         (X % Y) : ${Y !== 0 ? `${X} % ${Y} = ${X % Y}` : "Cannot divide by zero"}`);
+console.log("------------------------------");
+
+console.log("1. Addition (X + Y) :", X + " + " + Y + " = " + add);
+console.log("2. subtraction (X - Y) :", X + " - " + Y + " = " + sub);
+console.log("3. Multiplication (X * Y) :", X + " * " + Y + " = " + mul);
+console.log("4. Division (X / Y) :", X + " / " + Y + " = " + div);
+console.log("5. Modulus (X % Y) :", X + " % " + Y + " = " + mod);
